fce04d21bc27ab5afd0e3489a7465a06c28f3d4b
Tyler Gandy <tylerbgandy@gmail.com>
2022-04-17 13:32:42 -0400

    simplified code

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,7 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
-const reactionRoutes = require("./reactionRoutes");
 
 router.use("/thoughts", thoughtRoutes);
 router.use("/users", userRoutes);
-router.use("reactions", reactionRoutes);
 
 module.exports = router;
