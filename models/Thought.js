75a6adb51683c16c69232a543d1c0e92cc3878c6
Tyler Gandy <tylerbgandy@gmail.com>
2022-04-17 11:55:27 -0400

    changes made to thought.js and controller

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,34 @@
 // Require schema and model from mongoose
-const mongoose = require("mongoose");
+const { Schema, mongoose } = require("mongoose");
+const Reaction = require("./Reaction.js");
+
+const reactionSchema = new mongoose.Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
 
 // Construct a new instance of the schema class
 const thoughtSchema = new mongoose.Schema(
@@ -22,9 +51,22 @@ const thoughtSchema = new mongoose.Schema(
   }
 );
 
+thoughtSchema
+  .virtual("reactionCount")
+  // Getter
+  .get(function () {
+    return `${this.reactions.length}`;
+  })
+  // Setter to set the first and last name
+  .set(function (v) {
+    const first = v.split(" ")[0];
+    const last = v.split(" ")[1];
+    this.set({ first, last });
+  });
+
 // Using mongoose.model() to compile a model based on the schema 'bookSchema'
 const Thought = mongoose.model("Thought", thoughtSchema);
 
 const handleError = (err) => console.error(err);
 
-module.exports = User;
+module.exports = Thought;
